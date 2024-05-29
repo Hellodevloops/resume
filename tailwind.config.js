3f7caf2c8d8bf8712a6059d0238455fdd4dd1925
Richa <richa@devloops.in>
2024-05-29 12:42:14 +0530

    remove custom width

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,12 @@ module.exports = {
       backgroundImage: {
         dot: "url('/assets/dots.svg')",
       },
-      screens: {
-        '2xl': '2400px',  // Add a new breakpoint for screens larger than 2400px
-      },
-      maxWidth: {
-        'screen-2xl': '2400px',  // Add a new max-width utility for 2400px
-      },
+      // screens: {
+      //   '2xl': '2400px', 
+      // },
+      // maxWidth: {
+      //   'screen-2xl': '2400px', 
+      // },
     },
   },
   corePlugins: {
